4be61f9ea1152e845939a91a5210cc82ff424b8f
Alex Baldwin <alexbaldwin@gmail.com>
2022-11-09 17:19:28 -0600

    feed url

--- index.rb
+++ index.rb
@@ -5,7 +5,7 @@ get '/' do
 	"Hello World!"
 end
 
-get '/analogprocess.rss' do
+get '/feed.xml' do
 	content_type 'text/xml'
 	redis = Redis.new(url: "redis://:p119783d3705854696d68cbbe3e8218b1f2312d40dc40de730a7ff0ba065e9eed@ec2-35-172-46-76.compute-1.amazonaws.com:6999")
 	redis.get("rss")
